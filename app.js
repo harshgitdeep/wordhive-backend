7c299d034b3b1fad6a2737780d56f851c6112d37
Harshdeep Singh <88957566+harshgitdeep@users.noreply.github.com>
2024-05-03 09:36:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ const fs = require('fs');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
-
-app.use(cors({credentials:true,origin:'https://wordhive.vercel.app'}));
+// https://wordhive.vercel.app
+app.use(cors({credentials:true,origin:'*'}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
